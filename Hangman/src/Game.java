f449518fb02a7cef4c0aa40dc731a985e6c6797c
Anthony Wright <antowright@gmail.com>
2013-05-25 18:29:22 +0100

    Oops

--- Hangman/src/Game.java
+++ Hangman/src/Game.java
@@ -16,8 +16,9 @@ public class Game {
 		int numOfFailures=7;
 		
 		//temp word, will come from a file or database in the end
-		String word="Java";
-		String visibleWord="****";
+		String word="Love";
+//		String visibleWord="****";
+		StringBuilder visibleWord = new StringBuilder("----");
 //		String[] word={"J","a","v","a"};
 //		String[] visibleWord={"*","*","*","*"};
 		
@@ -33,42 +34,46 @@ public class Game {
 		//Run the input/output portion
 		while(runGame){
 			try{
-
-			System.out.println("Failures: " + failures + "\n");
-			System.out.println("Clue: " + visibleWord + "\n");
-			
-            System.out.print("Enter your guess?" + "\n");
-            Input.nextToken();
-            
-            //local variables
-            String guess = Input.sval;
-            guess = guess.toUpperCase();
-            guess = guess.substring(0,1);
-            
-            System.out.print("Your guess is:" + guess + "\n");
-
-    		//keeps track of most recent answers state
-    		boolean guessChecker = word.contains(guess);
-    		int index = word.indexOf(guess);
-    		if(index>=0)
-//    			replace
-            System.out.print("Is it in the word: " + guessChecker + "\n");
-            System.out.print("Index: " + index + "\n");
-            if(guessChecker==false)
-            	failures++;
-            
-            	//win state
-	            if(word == visibleWord)
-	            {
-	                System.out.print("You win\n");
-	                runGame=false;	            	
-	            }            
-            	//win state
-	            else if(failures == numOfFailures)
-	            {
-	                System.out.print("You lose\n");
-	                runGame=false;
-	            }
+					System.out.println("Failures: " + failures + "\n");
+					System.out.println("Clue: " + visibleWord.toString() + "\n");
+					
+		            System.out.print("Enter your guess?" + "\n");
+		            Input.nextToken();
+		            
+		            //local variables
+		            String guess = Input.sval;
+		            guess = guess.toUpperCase();
+		            guess = guess.substring(0,1);
+		            
+		            System.out.print("Your guess is:" + guess + "\n");
+		
+		    		//keeps track of most recent answers state
+		    		boolean guessChecker = word.contains(guess);
+		    		int index = word.indexOf(guess);
+		    		if(index>=0)
+		    			for (int i = word.indexOf(guess);
+		    				     i >= 0;
+		    				     i = word.indexOf(guess, i + 1))
+		    				{
+		    					visibleWord.setCharAt(i, guess.charAt(0));
+		    				}
+		            System.out.print("Is it in the word: " + guessChecker + "\n");
+//		            System.out.print(word.matches(visibleWord.toString()));
+		            if(guessChecker==false)
+		            	failures++;
+		            
+		            //win state
+			        if(word.matches(visibleWord.toString()))
+			        {
+			             System.out.print("You win\n");
+			             runGame=false;	            	
+			        }            
+		            //lose state
+			        if(failures == numOfFailures)
+			        {
+			             System.out.print("You lose\n");
+			             runGame=false;
+			        }
 			}
             catch (Exception e) {
                 e.printStackTrace();
